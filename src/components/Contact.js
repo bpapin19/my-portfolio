9bbb569baf279152ef9349a4c71999bfe3251d55
BPapin19 <bmpapin@yahoo.com>
2021-01-23 16:41:05 -0800

    updated for new domain

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,19 +25,5 @@ export default function Contact() {
                 </section>
             </div>
         </main>
-        // <main className="bg-blue-100 min-h-screen p-12">
-        //     <h1 className="text text-5xl flex justify-center">Contact Information</h1>
-        //     <div className="space-y-5">
-        //         <div className="ml-10">
-        //             <h2 className="font-bold">Email: </h2>
-        //             <div>bmpapin@csu.fullerton.edu <br />bmpapin@yahoo.com </div>
-        //         </div>
-        //         <div className="ml-10">
-        //             <h2 className="font-bold">Phone: </h2>
-        //             <div>(714) 328-6755</div>
-        //         </div>
-        //     </div>
-        // </main>
-    
     )
 }
